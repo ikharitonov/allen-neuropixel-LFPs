59f508eb6d23497c5fcf9a49633036055952ac65
ikharitonov <sarfins2101@gmail.com>
2023-06-13 16:08:49 +0200

    lfp file renaming

---
+++ rename_nwb_files.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+import shutil
+
+def mkdir(path):
+    if not os.path.exists(path): os.makedirs(path)
+    return path
+
+original_path = Path.home()/'Desktop'/'disk1'/'dandi_lfp'
+resulting_path = Path.home()/'Desktop'/'disk1'/'ecephys_data'
+
+files = os.listdir(original_path)
+
+for f in files:
+    if 'probe' not in f: # Session file
+        session_id = int(f.split('_')[1].split('0')[1].split('.')[0])
+        shutil.move(original_path/f, mkdir(resulting_path/f'session_{session_id}')/f'session_{session_id}.nwb')
+    else: # Probe file
+        session_id = int(f.split('_')[1].split('-')[1])
+        probe_id = int(f.split('_')[2].split('-')[1])
+        shutil.move(original_path/f, mkdir(resulting_path/f'session_{session_id}')/f'probe_{probe_id}_lfp.nwb')
+print('All files moved:')
+print('From',original_path)
+print('To',resulting_path)
