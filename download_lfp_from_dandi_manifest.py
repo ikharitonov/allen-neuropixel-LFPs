cc580289aa1282a1cff4261b6c8d9ce1f98615cf
Iakov K <sarfins2101@gmail.com>
2023-06-13 15:41:18 +0200

    downloading sessions

--- download_lfp_from_dandi_manifest.py
+++ download_lfp_from_dandi_manifest.py
@@ -2,24 +2,35 @@ import numpy as np
 import json
 import pickle
 import subprocess
+from pathlib import Path
+
+OUTPUT_DIR = Path.home()/'Desktop'/'dandi_lfp'
 
 manifest_path = 'dandi_manifest.json'
 with open(manifest_path, 'r') as f: data = json.load(f)
 with open('VISpm_VISp_probes.pkl', 'rb') as f:
     probe_list = pickle.load(f)
 
+session_list = np.array(probe_list)[:,0].flatten()
 probe_list = np.array(probe_list)[:,1:].flatten()
 
 data = data['results']
 
 for f in data:
-    session_name, probe_file = f['path'].split('/')
+    subject_name, probe_file = f['path'].split('/')
     if 'probe' in probe_file:
         probe_id = int(probe_file.split('_')[2].split('-')[1])
-        # dandi download the session file + the probe files from the list
+        # dandi download the probe files from the list
         if probe_id in probe_list:
             asset_id = f['asset_id']
-            result = subprocess.run(['dandi','download',f'https://api.dandiarchive.org/api/dandisets/000021/versions/draft/assets/{asset_id}/download/'], capture_output=True, text=True)
-            # result = subprocess.run(['dandi', '--version'], capture_output=True, text=True)
+            result = subprocess.run(['dandi','download','-o',OUTPUT_DIR,'-e','skip',f'https://api.dandiarchive.org/api/dandisets/000021/versions/draft/assets/{asset_id}/download/'], capture_output=True, text=True)
+            print(result.stdout)
+            print(result.stderr)
+    else:
+        session_id = int(probe_file.split('-')[-1].split('.')[0])
+        # Download the session file
+        if session_id in session_list:
+            asset_id = f['asset_id']
+            result = subprocess.run(['dandi','download','-o',OUTPUT_DIR,'-e','skip',f'https://api.dandiarchive.org/api/dandisets/000021/versions/draft/assets/{asset_id}/download/'], capture_output=True, text=True)
             print(result.stdout)
             print(result.stderr)
